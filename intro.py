19f5c03976c82f8afcb5c9611a98ea61b7d085d9
Liberbell <liberbell@gmail.com>
2020-04-19 10:14:19 +0900

    intro8

--- intro.py
+++ intro.py
@@ -1,5 +1,5 @@
 import socket
 
 print(socket.gethostname())
-print(socket.gethostbyname(socket.gethostname()))
+# print(socket.gethostbyname(socket.gethostname()))
 print(socket.gethostbyaddr("127.0.0.1"))
