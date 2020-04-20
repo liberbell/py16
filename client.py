9298c3ef94a97786ae132ca6aa6c14f884bc7faa
Liberbell <liberbell@gmail.com>
2020-04-20 09:14:11 +0900

    client3

--- client.py
+++ client.py
@@ -2,3 +2,8 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("localhost", 4571))
+
+msg = s.recv(1024)
+print("Message from server:", meg.decode("utf-8"))
+
+s.close()
