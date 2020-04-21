e0624011418873e8d3c6fa42928860d86358abae
Liberbell <liberbell@gmail.com>
2020-04-21 17:56:52 +0900

    server4

--- pythonobjects/server.py
+++ pythonobjects/server.py
@@ -3,6 +3,14 @@ from product import Product
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind("0.0.0.0", 4571)
+
     python_dictionary = {"a": 1, "b": 2}
-    
     custom_object = Product("P024", "Torch", 13)
+
+    s.listen(5)
+
+    print("Server is up. Listening for connections...")
+
+    client, address = s.accept()
+    print("Connection to", address, "established\n")
+    print("Client object", client, "\n")
