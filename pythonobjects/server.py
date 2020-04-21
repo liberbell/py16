7cef6b97056e8e552364125b1319c6d9f5ca8925
Liberbell <liberbell@gmail.com>
2020-04-21 17:53:55 +0900

    server3

--- pythonobjects/server.py
+++ pythonobjects/server.py
@@ -4,3 +4,5 @@ from product import Product
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind("0.0.0.0", 4571)
     python_dictionary = {"a": 1, "b": 2}
+    
+    custom_object = Product("P024", "Torch", 13)
