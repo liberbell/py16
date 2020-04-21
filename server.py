3cddb79043946b9525834da04fe510088f9f5590
Liberbell <liberbell@gmail.com>
2020-04-21 15:00:01 +0900

    server1

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
 s.bind(("0.0.0.0", 4571))
 s.listen(5)
