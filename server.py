24ff0ac53b0f7a65c2518f3f537dce28841986a0
Liberbell <liberbell@gmail.com>
2020-04-21 15:25:03 +0900

    server4

--- server.py
+++ server.py
@@ -5,12 +5,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
     s.bind(("0.0.0.0", 4571))
     s.settimeout(5)
-    s.listen(5)
 
-    print("Server is up. Listening for connections...")
+    try:
+        s.listen(5)
 
-    client, address = s.accept()
-    print("Connection to ...", address, "established\n")
-    print("Client Object:", client, "\n")
+        print("Server is up. Listening for connections...")
 
-    client.send(bytes("Hello! Welcome to socket programming.", "utf-8"))
+        client, address = s.accept()
+        print("Connection to ...", address, "established\n")
+        print("Client Object:", client, "\n")
+
+        client.send(bytes("Hello! Welcome to socket programming.", "utf-8"))
+
+    except:
+        print("Timeout has been execeeded. Close the connection...")
