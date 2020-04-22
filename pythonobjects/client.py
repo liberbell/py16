80073c69291a0aa964d06bc08958aac29edeac6b
Liberbell <liberbell@gmail.com>
2020-04-22 14:27:41 +0900

    client4

--- pythonobjects/client.py
+++ pythonobjects/client.py
@@ -3,3 +3,8 @@ from product import Product
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect(("localhost", 4571))
+
+    while True:
+        msg = s.recv(1024)
+        if not msg:
+            print("No message from the server.Closing the connection...")
