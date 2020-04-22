f9d18c30a3b5726f47911ad0fca00d06af12487f
Liberbell <liberbell@gmail.com>
2020-04-22 14:45:46 +0900

    server5

--- pythonobjects/server.py
+++ pythonobjects/server.py
@@ -6,6 +6,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind(("0.0.0.0", 4571))
 
     python_dictionary = {"a": 1, "b": 2}
+    picked_dictionary = pickle.dumps(python_dictionary)
     custom_object = Product("P024", "Torch", 13)
 
     s.listen(5)
