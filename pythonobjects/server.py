08fc51c0904a4ea376d3e381cdb1c2ad76a6bc74
Liberbell <liberbell@gmail.com>
2020-04-23 17:46:04 +0900

    server6

--- pythonobjects/server.py
+++ pythonobjects/server.py
@@ -5,8 +5,8 @@ import pickle
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind(("0.0.0.0", 4571))
 
-    python_dictionary = {"a": 1, "b": 2}
-    pickled_dictionary = pickle.dumps(python_dictionary)
+    # python_dictionary = {"a": 1, "b": 2}
+    # pickled_dictionary = pickle.dumps(python_dictionary)
 
     custom_object = Product("P024", "Torch", 13)
     pickled_object = pickle.dumps(custom_object)
