51959d9614f17c0ca00b897b9255a47cc68e1dc5
Liberbell <liberbell@gmail.com>
2020-04-23 16:06:30 +0900

    server3

--- pythonobjects/server.py
+++ pythonobjects/server.py
@@ -6,13 +6,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind(("0.0.0.0", 4571))
 
     python_dictionary = {"a": 1, "b": 2}
-    pickeld_dictionary = pickle.dumps(python_dictionary)
+    pickled_dictionary = pickle.dumps(python_dictionary)
 
     custom_object = Product("P024", "Torch", 13)
     pickled_object = pickle.dumps(custom_object)
 
     print("Serialized dictionary type:", type(pickled_dictionary))
-    print("Serialized object type:", type(pickeld_object))
+    print("Serialized object type:", type(pickled_object))
 
     s.listen(5)
 
